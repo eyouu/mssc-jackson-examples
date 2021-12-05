f5ed8b351c42f9b3ee399b37d68139e46948f867
Vasyl Zhovnir <vasyl.zhovnir@cgm.com>
2021-12-05 16:41:17 +0200

    minor updates

--- src/main/java/guru/springframework/msscjacksonexamples/model/BeerDTO.java
+++ src/main/java/guru/springframework/msscjacksonexamples/model/BeerDTO.java
@@ -2,6 +2,8 @@ package guru.springframework.msscjacksonexamples.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +13,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -36,7 +39,12 @@ public class BeerDTO {
     @JsonFormat(shape = JsonFormat.Shape.STRING) // now this field will be serialized as String type.
     private BigDecimal price;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH", shape = JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     private OffsetDateTime lastUpdatedDate;
+
+    // Using customer serializer/deserializer
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate myLocalDate;
 }
--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDTOTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDTOTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.autoconfigure.json.JsonTest;
 @JsonTest
 class BeerDTOTest extends BaseTest {
 
-    private static final String TEST_JSON = "{\"beerId\":\"8893caf8-bf06-48d0-9eaf-c10dedb18d71\",\"beerName\":\"beerName\",\"beerStyle\":\"Ale\",\"upc\":123124,\"price\":12.99,\"createdDate\":\"2021-12-05T15:59:50.9572453+02:00\",\"lastUpdatedDate\":\"2021-12-05T15:59:50.9582424+02:00\"}";
+    private static final String TEST_JSON = "{\"beerName\":\"beerName\",\"beerStyle\":\"Ale\",\"upc\":123124,\"price\":\"12.99\",\"createdDate\":\"2021-12-05T16:39:17+0200\",\"lastUpdatedDate\":\"2021-12-05T16:39:17.6155188+02:00\",\"myLocalDate\":\"20211205\",\"beerId\":\"8893caf8-bf06-48d0-9eaf-c10dedb18d71\"}";
 
     @Autowired
     private ObjectMapper objectMapper;
